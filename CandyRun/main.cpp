dc22bc46b67e304a9501d0c0aba38ee6b12bc022
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-04-30 17:09:44 +0200

    Test

--- CandyRun/main.cpp
+++ CandyRun/main.cpp
@@ -7,10 +7,11 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!\n";
+    cout << "Hello, Candy!\n";
     return 0;
 }
