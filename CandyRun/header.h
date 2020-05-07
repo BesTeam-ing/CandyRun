fa17706e9a25b524e859f7ab9ddf7cf88b71cc00
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-05-07 18:15:33 +0200

    basic project

---
+++ CandyRun/header.h
@@ -0,0 +1,24 @@
+//
+//  header.h
+//  CandyRun
+//
+//  Created by Ciro De Vita on 05/05/2020.
+//  Copyright © 2020 BesTeam-ing. All rights reserved.
+//
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <OpenGL/glext.h>
+
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+#include <ctime>
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+#define W 1280
+#define H 720
--- CandyRun/main.cpp
+++ CandyRun/main.cpp
@@ -1,17 +1,64 @@
-//
-//  main.cpp
-//  CandyRun
-//
-//  Created by Gennaro Mellone and Ciro Giuseppe De Vita on 30/04/2020.
-//  Copyright © 2020 BesTeam-ing. All rights reserved.
-//
+#include "header.h"
 
-#include <iostream>
+// Drawing routine.
+void drawScene(void)
+{
+    // clear (has to be done at the beginning)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    
+    glColor3f(0.0, 255.0, 0.0); //colore
 
-using namespace std;
+    //disegna un poligono con vertici specificati
+    glBegin(GL_POLYGON); //inizio
+        glVertex3f(20.0, 20.0, 0.0);
+        glVertex3f(80.0, 20.0, 0.0);
+        glVertex3f(80.0, 80.0, 0.0);
+        glVertex3f(20.0, 80.0, 0.0);
+    glEnd();
+    
+    // swap buffers (has to be done at the end)
+    glutSwapBuffers();
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, Candy!\n";
+// Initialization routine.
+void setup(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glViewport(0, 0, W, H);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0f, W, 0.0f, H, 0.0f, 1.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+// OpenGL window reshape routine.
+void resize(int w, int h)
+{
+    glutReshapeWindow(W, H);
+}
+
+int main(int argc, char **argv)
+{
+    // initialize opengl (via glut)
+    glutInit(&argc, argv);
+    glutInitContextVersion(4, 3);
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(W, H);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Candy Run");
+
+    // Register callback functions
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(resize);
+
+    setup();
+
+    // start the whole thing
+    glutMainLoop();
     return 0;
 }
+
