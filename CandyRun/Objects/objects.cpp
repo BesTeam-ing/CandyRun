418f45ae2b5716e1c48420a49fa0be4ce390dbb3
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-05-13 14:26:51 +0200

    code restyling

--- CandyRun/Objects/objects.cpp
+++ CandyRun/Objects/objects.cpp
@@ -8,60 +8,42 @@
 
 #include "objects.hpp"
 
-Road::Road(){}
-Road::~Road(){}
+std::vector<Object> objects;
 
-void Road::init(const char *t){
-    texture = SOIL_load_OGL_texture(t, SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+Object::Object(){}
+Object::~Object(){}
+
+float Object::getX(){
+    return this->pos_X;
 }
 
-void Road::draw(){
-    glColor4f(0.8, 0.8, 0.8, 0.1);
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, this->texture);
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
-    glPushMatrix();
-    glBegin(GL_QUADS);
-        glTexCoord2f(0.0f,0.0f);
-        glVertex3f(pos_X + start_X, pos_Y + start_Y, start_Z + pos_Z);
-        glTexCoord2f(0.0f,1);
-        glVertex3f(pos_X + start_X, pos_Y + start_Y, end_Z + pos_Z);
-        glTexCoord2f(1.0f,1);
-        glVertex3f(pos_X + end_X, pos_Y + end_Y, end_Z + pos_Z);
-        glTexCoord2f(1.0f,0.0f);
-        glVertex3f(pos_X + end_X, pos_Y + end_Y, start_Z + pos_Z);
-    glEnd();
-    glPopMatrix();
-    glDisable(GL_TEXTURE_2D);
+float Object::getY(){
+    return this->pos_Y;
 }
 
-void Road::setPosition(float X, float Y, float Z){
+float Object::getZ(){
+    return this->pos_Z;
+}
+
+unsigned int Object::getTexture(){
+    return this->texture;
+}
+
+bool Object::getObject(){
+    return this->isEnemy;
+}
+
+void Object::setPosition(float X, float Y, float Z){
     this->pos_X = X;
     this->pos_Y = Y;
     this->pos_Z = Z;
 }
 
-float Road::getX(){
-    return this->pos_X;
-}
-float Road::getY(){
-    return this->pos_Y;
-}
-float Road::getZ(){
-    return this->pos_Z;
-}
-unsigned int Road::getTexture(){
-    return this->texture;
+void Object::setObject(bool obj){
+    this->isEnemy = obj;
 }
 
-Enemy::Enemy(){}
-Enemy::~Enemy(){}
-
-void Enemy::draw(){
-
+void Object::draw(){
     glColor4f(0.8, 0.8, 0.8, 0.1);
     
     glPushMatrix();
@@ -70,58 +52,40 @@ void Enemy::draw(){
     glPopMatrix();
 
 }
-void Enemy::setPosition(float X, float Y, float Z){
-    this->pos_X = X;
-    this->pos_Y = Y;
-    this->pos_Z = Z;
-}
 
-float Enemy::getX(){
-    return this->pos_X;
-}
-float Enemy::getY(){
-    return this->pos_Y;
-}
-float Enemy::getZ(){
-    return this->pos_Z;
-}
-unsigned int Enemy::getTexture(){
-    return this->texture;
-}
-
-
-bool Enemy::getEnemy(){
-    return this->isEnemy;
-}
-
-void Enemy::setEnemy(bool obj){
-    this->isEnemy = obj;
-}
-
-// OTHER FUNCTIONS
-
-std::vector<Road> roads;
-
-void initializeGround(){
-    Road r;
+void Object::initObject(){
+    Object o;
     
-    float beginning = -90.0;
+    float beginning = -60.0;
     for(int i=0; i<4; i++){
-        r.init("textures/road.jpg");
-        r.setPosition(0.0f, 0.0f, beginning);
-        roads.push_back(r);
-        beginning += 30;
+        int n = rand()%10-5;
+        o.setPosition(n, 1.0f, beginning);
+        objects.push_back(o);
+        
+        beginning += 20;
     }
 }
 
-void drawRoad(Road &road){
-    for (int i=0; i < roads.size(); i++){
-        if(roads[i].getZ() >= 30.0f)
-            roads[i].setPosition(0.0f, 0.0f, -89.9f);
+void Object::drawObject(){
+    int n = rand()%10-5;
+    
+    for (int i=0; i < objects.size(); i++){
+        if(objects[i].getZ() > 30.0){
+            objects[i].setPosition(n, 1, -60);
+        }
         
         glPushMatrix();
-            roads[i].setPosition(0.0f, 0.0f, roundf((roads[i].getZ() + 0.1f)*100)/100);
-            roads[i].draw();
+            objects[i].setPosition(objects[i].getX(), objects[i].getY(), objects[i].getZ() + 0.1);
+            objects[i].draw();
         glPopMatrix();
+    }
+}
+
+void Object::handleCollision(float x, float y, float z){
+    for (int i=0; i < objects.size(); i++){
+        if((objects[i].getZ() <= 15.0f && objects[i].getZ() >= 14.9f) && (objects[i].getX() <= x + 0.5 && objects[i].getX() >= x - 0.5)){
+            if(objects[i].getObject())
+                std::cout<<"Collision"<<std::endl;
+        }
     }
 }
--- CandyRun/Objects/objects.hpp
+++ CandyRun/Objects/objects.hpp
@@ -16,131 +16,28 @@
 #include <math.h>
 #include <vector>
 #include <iomanip>
-
-#include "Vector3f.h"
-
 #include <iostream>
 
-#define DEG2RAD(a) (a*0.0174532925)
-// CLASSES
-class Camera
-{
+class Object{
+
 public:
-    Vector3f eye, center, up;
-
-    Camera(float eyeX = 0.0f, float eyeY = 12.0f, float eyeZ = 260.0f, float centerX = 0.0f, float centerY = 0.0f, float centerZ = 240.0f, float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f)
-    {
-        eye = Vector3f(eyeX, eyeY, eyeZ);
-        center = Vector3f(centerX, centerY, centerZ);
-        up = Vector3f(upX, upY, upZ);
-    }
-
-    void moveX(float d)
-    {
-        
-        Vector3f right = up.cross(center - eye).unit();
-        Vector3f r_d = right * d;
-        
-        eye = eye + r_d;
-        center = center + r_d;
-    }
-
-    void moveY(float d)
-    {
-        Vector3f u_d = up.unit() * d;
-        eye = eye + u_d;
-        center = center + u_d;
-    }
-
-    void moveZ(float d)
-    {
-        Vector3f view = (center - eye).unit();
-        Vector3f v_d = view * d;
-        
-        eye = eye + v_d;
-        center = center + v_d;
-    }
-
-    void rotateX(float a)
-    {
-        Vector3f view = (center - eye).unit();
-        Vector3f right = up.cross(view).unit();
-        Vector3f c1 = view * cos(DEG2RAD(a));
-        Vector3f c2 =up * sin(DEG2RAD(a));
-        
-        view = c1 + c2;
-        up = view.cross(right);
-        center = eye + view;
-    }
-
-    void rotateY(float a)
-    {
-        Vector3f view = (center - eye).unit();
-        Vector3f right = up.cross(view).unit();
-        Vector3f c1 = view * cos(DEG2RAD(a));
-        Vector3f c2 =right * sin(DEG2RAD(a));
-        
-        view = c1 + c2;
-        right = view.cross(up);
-        center = eye + view;
-    }
-
-    void look()
-    {
-        gluLookAt(
-            eye.x, eye.y, eye.z,
-            center.x, center.y, center.z,
-            up.x, up.y, up.z);
-    }
-};
-
-class Road
-{
+    Object();
+    ~Object();
     
-private:
-    unsigned int texture;
-    float start_X = -7.0;
-    float start_Y = 0.0;
-    float start_Z = -15.0;
-    
-    float end_X = 7.0;
-    float end_Y = 0.0;
-    float end_Z = 15.0;
-    
-    float pos_X = 0;
-    float pos_Y = 0;
-    float pos_Z = 0;
-    
-public:
-    bool alive = true;
-    
-    Road();
-    ~Road();
-    
-    void init(const char* t);
-    void draw();
     void setPosition(float X,float Y,float Z);
+    
     float getX();
     float getY();
     float getZ();
+    
     unsigned int getTexture();
-};
-
-
-class Enemy{
-
-public:
-    Enemy();
-    ~Enemy();
-
-    void draw();
-    void setPosition(float X,float Y,float Z);
-    float getX();
-    float getY();
-    float getZ();
-    unsigned int getTexture();
-    bool getEnemy();
-    void setEnemy(bool obj);
+    
+    bool getObject();
+    void setObject(bool obj);
+    
+    void initObject();
+    void drawObject();
+    void handleCollision(float x, float y, float z);
     
 private:
     unsigned int texture;
@@ -157,11 +54,8 @@ private:
     float pos_Z = -30;
     
     bool isEnemy = true;
+    
+    void draw();
 };
-//OTHER FUNCTIONS
-
-
-void initializeGround();
-void drawRoad(Road &road);
 
 #endif /* objects_hpp */
